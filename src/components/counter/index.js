c530b9ac809e62682c83e2222c4509a6e44d116b
robdcampbell <rdeancampbell@gmail.com>
2021-01-07 09:23:12 -0500

    Basic counter in place

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,16 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {}, []);
+  const handleCount = (e) => {
+    const counterButton = e.target.id;
+
+    if (counterButton === "inc") {
+      setCount((current) => (current += 1));
+    } else if (counterButton === "dec") {
+      setCount((current) => (current -= 1));
+    } else if (counterButton === "reset") {
+      setCount(0);
+    }
+    return;
+  };
 
   return (
     <div>
-      <h2>Another counter</h2>
-      <p value={count}>Counter set to: {count}</p>
-      <button> + </button>
-      <button> - </button>
+      <h1>Counter.</h1>
+      <p>Current count is: {count}</p>
+      <button onClick={handleCount} id="inc">
+        +
+      </button>
+      <button onClick={handleCount} id="dec">
+        -
+      </button>
+      <button onClick={handleCount} id="reset">
+        reset
+      </button>
     </div>
   );
 };
